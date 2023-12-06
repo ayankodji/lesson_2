b35abd7c7b1fcc933e0ebea5f9e05f5401474830
AsanovNurik <amir.khan.2007@mail.ru>
2023-12-06 08:55:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int number = 7;
+        switch (number) {
+            case 1:
+                System.out.println("число равно одному");
+                break;
+            case  2:
+                System.out.println("число равно двум");
+                break;
+            case  3:
+                System.out.println("число равно трем");
+                break;
+            default:
+                System.out.println("число неизвестно");
+                break;
+        }
+
+    }
+}
